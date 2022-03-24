7dd0a43aae4aa898551ec55155ac27311285654e
NeverMind109 <vladgladkih123@gmail.com>
2022-03-24 18:54:16 +0300

    Created: Added Header

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,41 @@
 $(function () {
-    
-})
+  $(".menu-button").on("click", function () {
+    $(".menu").toggleClass("menu--active");
+    $("body").toggleClass("overflow-hidden");
+    let link = $(".menu-list__link");
+    let logo = $(".logo");
+    let profile = $(".profile");
+    let search = $(".search");
+    let cart = $(".cart");
+    function click(element) {
+      element.on("click", function () {
+        $(".menu").removeClass("menu--active");
+        $("body").removeClass("overflow-hidden");
+      });
+    }
+    click(link);
+    click(logo);
+    click(profile);
+    click(search);
+    click(cart);
+  });
+
+  var w = $(window).width();
+  if (w <= 576) {
+    $(".dropdown-carret").removeClass("dropdown").addClass("dropend");
+  }
+
+  $(window).on("resize", function () {
+    var win = $(this); //this = window
+    if (win.width() <= 576) {
+      if ($(".dropdown-carret").hasClass("dropdown")) {
+        $(".dropdown-carret").removeClass("dropdown").addClass("dropend");
+      }
+    }
+    if (win.width() > 576) {
+      if (!$(".dropdown-carret").hasClass("dropdown")) {
+        $(".dropdown-carret").removeClass("dropend").addClass("dropdown");
+      }
+    }
+  });
+});
